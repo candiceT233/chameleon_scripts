99ee1266eeca6ca6eaac6101ecde58319de6eb86
candiceT233 <candicetang233@gmail.com>
2023-06-27 15:52:31 -0500

    script ready, need image

--- 1kgenome_tier_scripts/bm_instance-1kg-tier.sh
+++ 1kgenome_tier_scripts/bm_instance-1kg-tier.sh
@@ -53,9 +53,10 @@ starting_commands="
 sudo yum update -y
 cd ~
 git clone https://gitlab.pnnl.gov/perf-lab/bigflowtools/datalife.git
-cd datalife 
-bash artifacts_sc23/scripts/install_env_dep.sh
+cd datalife/artifacts_sc23/scripts
+bash install_env_dep.sh
 wait
+bash run.sh # generate stats
 
 cd /home/cc/datalife/evaluation_scripts/performance/1000genome_plot/1000genome_perf_number
 bash perf-test.sh
