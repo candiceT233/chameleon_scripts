ed62790ee4d870791d497cd72f5bdc43cb6ccf8d
MengTang <candicetang233@gmail.com>
2023-06-27 22:06:41 -0500

    fix to run on Mac and Linux

--- 1kgenome_tier_scripts/floating_ip_to_insatnce.sh
+++ 1kgenome_tier_scripts/floating_ip_to_insatnce.sh
@@ -20,8 +20,9 @@ fip_to_instance () {
     openstack floating ip create public | tee $fip_info
     echo " Floating IP information saved to $fip_info"
 
-    floating_ip_address=$(grep -oP "(?<=floating_ip_address \| )\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" "$fip_info")
-    # echo floating_ip_address: $floating_ip_address
+    # floating_ip_address=$(grep -oP "(?<=floating_ip_address \| )\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" "$fip_info")
+    floating_ip_address=$(grep -oE 'floating_ip_address \| ([0-9]{1,3}\.){3}[0-9]{1,3}' "$fip_info" | awk '{print $NF}')
+    echo "IP Address: $floating_ip_address"
 
     openstack server add floating ip "$instance_name" "$floating_ip_address"
     echo "Now you can ssh into the instance using the following command:"
