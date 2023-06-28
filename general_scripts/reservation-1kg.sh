118fd389548b41c2ad9e42e0bbefe4424eaaed0a
candiceT233 <candicetang233@gmail.com>
2023-06-27 22:13:33 -0500

    deted Mac or Linux for UTC time format

--- general_scripts/reservation-1kg.sh
+++ general_scripts/reservation-1kg.sh
@@ -44,10 +44,22 @@ if [[ ! " ${valid_node_types[@]} " =~ " $note_type " ]]; then
     exit 1
 fi
 
-# Get the current UTC time and add 10 minutes and 2 hours
-current_time=$(date -u +"%Y-%m-%d %H:%M")
-start_time=$(date -u -v+1M +'%Y-%m-%d %H:%M')
-end_time=$(date -u -v+"$lease_hours"H +'%Y-%m-%d %H:%M')
+if [[ "$OSTYPE" == *darwin* ]]; then
+    # Get the current UTC time and add 1
+    current_time=$(date -u +"%Y-%m-%d %H:%M")
+    start_time=$(date -u -v+1M +'%Y-%m-%d %H:%M')
+    end_time=$(date -u -v+"$lease_hours"H +'%Y-%m-%d %H:%M')
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+    # Get the current UTC time and add 1 minutes
+    current_time=$(date -u +'%Y-%m-%d %H:%M')
+    start_time=$(date -u -d '+1 minutes' +'%Y-%m-%d %H:%M')
+    end_time=$(date -u -d "+$lease_hours hours" +'%Y-%m-%d %H:%M')
+else
+    echo "Please hardcode the current UTC time, \"start_time\" and \"end_time\" in script."
+    echo "Time format: \"YYYY-MM-DD HH:MM\""
+fi
+
+
 
 # Display the current UTC time, start time, and end time
 echo "Current UTC time: $current_time"
